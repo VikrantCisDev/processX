374c1eae43c91fe50ac9183215e64e5819cc6a4f
vikrantcisdev <vikrant.raj@cisinlabs.com>
2026-03-09 16:50:51 +0530

    fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // fontFamily: {
+      //   Roboto: ['Roboto', 'sans-serif'],
+      // },
+    },
   },
   plugins: [],
 }
